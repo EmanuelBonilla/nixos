48afacda49af8cd1560700a3187206b3effdda3b
anthe <eldiegoemanuel@gmail.com>
2025-12-14 18:12:46 -0500

    feat use variable static for fast tests

--- system/network.nix
+++ system/network.nix
@@ -4,12 +4,5 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   networking.firewall.enable = false;
   networking.networkmanager.enable = true;
-  networking.hostName =
-    let
-      hostNameEnv = builtins.getEnv "HOSTNAME_TO_SET";
-    in
-    if hostNameEnv == "" then
-      builtins.throw "Environment variable HOSTNAME_TO_SET is not set"
-    else
-      hostNameEnv;
+  networking.hostName = "SAMD-E1";
 }
