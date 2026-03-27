ca2529d631c30f050d4fa64c757a4ab9eb134ad8
anthe <eldiegoemanuel@gmail.com>
2026-03-26 21:31:48 -0500

    feat: avoid logs of system init
    also avoid boot loader ignore all about that
    this is for a improvement  on use of this

--- host/boot.nix
+++ host/boot.nix
@@ -4,4 +4,15 @@
     enable = true;
     device = "/dev/sda";
   };
+  boot.loader.timeout = 0;
+  boot.plymouth = {
+    enable = true;
+    theme = "bgrt";
+  };
+  boot.initrd.systemd.enable = true;
+  boot.kernelParams = [
+    "quite"
+    "splash"
+    "rd.systemd.show_status=false"
+  ];
 }
